b3bc4410a1f61e7a691fdf4455a6ffbd51ebb9ee
Khoi <leanhkhoi2611@gmail.com>
2022-09-16 09:22:35 +0700

    Install express and show a welcome message

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-app.use(cors);
-app.use(express.json);
+app.use(cors());
+app.use(express.json());
 
-const port = 3000
 
 app.get('/', (req, res) => {
   res.json({message: "Welcome to contact book application."});
+  // res.send("Hello");
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports = app;
